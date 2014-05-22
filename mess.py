0b713b655a85d7724e1d413d9181b4be4fa3bd66
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2014-05-22 15:40:08 +0200

    Serialization to json

--- mess.py
+++ mess.py
@@ -8,32 +8,42 @@ import argparse
 import os
 import json
 
-parser = argparse.ArgumentParser(description = "MESS - Microsoft Excel Style-extractor and Styler")
+parser = argparse.ArgumentParser(description = "MESS - Microsoft Excel Style-extractor and Styler", usage = "mess.py ")
 parser.add_argument('--input', '-i',
-                    help = "Input file(s)", 
-                    nargs = '+',
+                    help = "Input XLS file to read styles from", 
                     required = True)
+parser.add_argument('--output', '-o',
+                    help = "Output XLS file to apply styles to", 
+                    required = False)
+parser.add_argument('--serialize', '-s',
+                    help = "Output XLS file to apply styles to", 
+                    required = False)
 
 args = parser.parse_args()
 
-# print args.input
+# print args.input, args.serialize
 
 # Styles data structure
 # List of workbook sheets
 # Each sheet is a dictionary, whose keys are coord tuples and values are styles
 bookStyles = []
 
-for f in args.input:
-    book = open_workbook(f, formatting_info = 1)
-    styles = Styles(book)
-    for i in range(book.nsheets):
-        sheetStyles = {}
-        sheet = book.sheet_by_index(i)
-        for i in range(sheet.nrows):
-            for j in range(sheet.ncols):
-                sheetStyles[(i,j)] = styles[sheet.cell(i,j)].name
-        bookStyles.append(sheetStyles)
+# Since json won't allow to serialize tuples as keys, we define this remap
+def remap_keys(mapping):
+    remap = []
+    for sheet in mapping:
+        remap.append([{'key':k, 'value': v} for k, v in sheet.iteritems()])
+    return remap
 
-print(json.dumps(bookStyles, indent=4))
-        
+book = open_workbook(args.input, formatting_info = 1)
+styles = Styles(book)
+for i in range(book.nsheets):
+    sheetStyles = {}
+    sheet = book.sheet_by_index(i)
+    for i in range(sheet.nrows):
+        for j in range(sheet.ncols):
+            sheetStyles[(i,j)] = styles[sheet.cell(i,j)].name
+    bookStyles.append(sheetStyles)
 
+with open(args.serialize, 'w') as outfile:
+    json.dump(remap_keys(bookStyles), outfile)
